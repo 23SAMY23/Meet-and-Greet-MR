8117eba0c96572f14537db6d86e201d4d6bffb7f
23SAMY23 <44160136+23SAMY23@users.noreply.github.com>
2022-02-06 13:33:52 +0100

    Property of TU Ilmenau

---
+++ Meet & Greet MR (VR)/Assets/Assets/Joystick Pack/Examples/JoystickPlayerExample.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickPlayerExample : MonoBehaviour
+{
+    public float speed;
+    public VariableJoystick variableJoystick;
+    public VariableJoystick RotationJoystick;
+    public CharacterController cc;
+    public Vector3 impact = Vector3.zero;
+    public GameObject mainavatar;
+
+    public void FixedUpdate()
+    {
+        Vector3 direction = Vector3.forward * variableJoystick.Vertical + Vector3.right * variableJoystick.Horizontal;
+        direction *= speed;
+
+        cc.Move (direction * Time.fixedDeltaTime);
+
+        Vector3 target = Vector3.up * RotationJoystick.Horizontal;
+        target *= speed * Time.fixedDeltaTime;
+        mainavatar.transform.Rotate(target);
+       // cc.transform.rotation(target * Time.fixedDeltaTime);
+    }
+}
